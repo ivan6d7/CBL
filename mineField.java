5e5d2357a974384c67efbc593047f90433ad5275
ivan6d7 <ivan7d6@gmail.com>
2025-10-13 18:20:50 +0200

    first commit

---
+++ mineField.java
@@ -0,0 +1,71 @@
+import java.util.Random;
+
+/**
+ * mineField.
+ * 
+ * @author Ivan Vnukov
+ * @id 2303663
+ * @author Maria Haritonova
+ * @id 2331705
+ */
+public class mineField {
+
+    int length;
+    int height;
+    int[][] field;
+
+
+    /*
+     * Create a minefield of size length * height and populate it with zeroes
+     */
+    private mineField(int length, int height) {
+        this.length = length;
+        this.height = height;
+        this.field = new int[length][height];
+        for (int[] i: this.field) {
+            for (int k: i) {
+                k = 0;
+            }
+        }
+    }
+
+    private int[][] populate (int mineCount) {
+
+        int[][] field = this.field;
+
+        Random random = new Random();
+        
+        int height = field.length; //height of the field
+        int width = field[0].length; //length of the field
+
+        for (int x = 0; x < mineCount; x++) { //generaten mineCount mines on the field
+            int a = random.nextInt(height - 1); //x-index of the potential mine
+            int b = random.nextInt(width - 1); //y-index of the potential mine
+
+            if (field[a][b] != 1) {
+                field[a][b] = 1;
+            } else {
+                x--;
+            }
+
+        }
+
+        return field;
+    }
+
+    private void print() {
+        for (int[] x: field) {
+            for (int y : x) {
+                System.out.print(y);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        mineField mineField = new mineField(10, 10);
+        mineField.populate(30);
+        mineField.print();
+    }
+
+}
